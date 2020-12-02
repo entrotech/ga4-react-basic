e08d01b139b1424beb5d296d51fd8d39518fa4ea
John Darragh <darragh@entrotech.net>
2020-12-02 09:13:56 -0800

    Added custom event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import analytics from "./hooks/analytics";
 
 function Home() {
-  return <div>This is the Home page</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button
+        onClick={() =>
+          analytics.sendEvent({
+            category: "General",
+            action: "click",
+            label: "Click Me",
+            value: 7,
+          })
+        }
+      />
+    </div>
+  );
 }
 
 export default Home;
